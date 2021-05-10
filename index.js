4082ef42af518f096900014873e777edba0eb19b
sunjus <sunju.shin1108@gmail.com>
2021-05-11 00:40:52 +0300

    ex 3.8* Phonebook backend step8 : configure morgan

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
