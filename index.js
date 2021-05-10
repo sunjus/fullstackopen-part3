28088b296e896d9b2bee978c6e0d412559a28451
sunjus <sunju.shin1108@gmail.com>
2021-05-11 00:45:47 +0300

    ex 3.9 phonebook backend step9 : cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+app.use(cors());
 
 let persons = [
   {
