7fa2920600fb33c752bbaab494b07ad34689e44c
sunjus <sunju.shin1108@gmail.com>
2021-05-13 17:41:15 +0300

    ex 3.11 phonebook full stack : Added build folder_frontend deploy

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ morgan.token("body", function (req, res) {
 });
 
 app.use(express.json());
+
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+
 app.use(cors());
+app.use(express.static("build"));
 
 let persons = [
   {
@@ -51,11 +54,18 @@ app.get("/api/persons", (request, response) => {
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   const person = persons.find((person) => person.id === id);
+  /*
   if (person) {
     response.json(person);
   } else {
     response.status(404).end();
   }
+  */
+  person
+    ? response.json(person)
+    : response
+        .status(404)
+        .json({ error: `id:${id} does not exist in the list.` });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -86,5 +96,6 @@ app.post("/api/persons", (request, response) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
